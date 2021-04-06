3e5a5fe7107c302b2d057b5467825d934248ae05
leofravega51 <leo-fravega@hotmail.com>
2021-04-06 00:05:17 -0300

    All commit

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ bot.start((ctx) =>
   )
 );
 
-// bot.hears("clima", async (ctx) => {
-//   const clima = await axios
-//     .get(`https://api.openweathermap.org/data/2.5/weather?lat=${process.env.LAT}&lon=${process.env.LONG}&appid=${process.env.API_KEY}`)
-//     .then((response) => {
-//       return response.data;
-//     })
-//     .catch((error) => console.log("Error de API", error));
+bot.hears("clima", async (ctx) => {
+  const clima = await axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${process.env.LAT}&lon=${process.env.LONG}&appid=${process.env.API_KEY}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => console.log("Error de API", error));
 
-//   return ctx.reply(`La temperatura hoy en ${clima.name} es de ${kelvinToCelsius(clima.main.temp)}`);
-// });
+  return ctx.reply(`La temperatura hoy en ${clima.name} es de ${kelvinToCelsius(clima.main.temp)}`);
+});
 
 
 const job = schedule.scheduleJob('00 00 08 * * *', async () => {
